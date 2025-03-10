3ca31f19be9e8a3c6c52034bfd4bf5ea1a340063
mcdicome <mcdogico@outlook.com>
2025-03-11 00:20:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,13 @@ from flask import Flask, Response
 from model import download_data, format_data, train_model, get_inference
 from config import model_file_path, TOKEN, TIMEFRAME, TRAINING_DAYS, REGION, DATA_PROVIDER
 
-
 app = Flask(__name__)
-
 
 def update_data():
     """Download price data, format data and train model."""
-    files = download_data(TOKEN, TRAINING_DAYS, REGION, DATA_PROVIDER)
-    format_data(files, DATA_PROVIDER)
+    df = download_data(TOKEN, TRAINING_DAYS, REGION, DATA_PROVIDER)
+    format_data(df, DATA_PROVIDER)
     train_model(TIMEFRAME)
-
 
 @app.route("/inference/<string:token>")
 def generate_inference(token):
@@ -27,7 +24,6 @@ def generate_inference(token):
     except Exception as e:
         return Response(json.dumps({"error": str(e)}), status=500, mimetype='application/json')
 
-
 @app.route("/update")
 def update():
     """Update data and return status."""
@@ -36,7 +32,6 @@ def update():
         return "0"
     except Exception:
         return "1"
-
 
 if __name__ == "__main__":
     update_data()
